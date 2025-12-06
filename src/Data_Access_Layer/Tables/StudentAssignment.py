6c55216bc08989ade109c84763b5fa4965001d84
Your Name <i.elhajj@hotmail.com>
2025-12-06 18:48:05 +0000

    Add Assignment and StudentAssignmentTable

---
+++ src/Data_Access_Layer/Tables/StudentAssignment.py
@@ -0,0 +1,25 @@
+from sqlalchemy import (
+    Column, Integer, String, Date, Time, ForeignKey, Text, DateTime, UniqueConstraint, FLOAT,BOOLEAN
+)
+from sqlalchemy.orm import relationship
+from src.Data_Access_Layer.Base import Base
+from src.Data_Access_Layer.Tables.Student import Student 
+
+
+# Junction table: Student Assignment
+class StudentAssignment(Base):
+    __tablename__ = "student_assignments"
+
+
+    st_as_id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey("students.student_id"))
+    assignment_id = Column(Integer, ForeignKey("assignments.assignment_id"))
+    submitted_date = Column(DateTime)
+    grade = Column(FLOAT)
+
+
+    __table_args__ = (UniqueConstraint("student_id", "assignment_id", name="uq_student_assignment"),)
+
+
+    student = relationship("Student", back_populates="student_assignments")
+    assignment = relationship("Assignment", back_populates="student_assignments")
